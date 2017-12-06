ee9588359e3cf6a834b05035b284cd40baa22202
azarovalex <alex@azarov.by>
2017-12-07 00:07:24 +0300

    Initial Commit

---
+++ Wheathered/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Wheathered
+//
+//  Created by Alex Azarov on 07/12/2017.
+//  Copyright © 2017 Alex Azarov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
